3eee79aefd4853fa3eeac52a3c50131e0686a11e
Aruna kadugu <arunakumari.7k@gmail.com>
2017-10-12 09:32:57 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+	int mynumber = 2;
+	double mothernumber = 1.7938;
+	System.out.println("mynumber:");
+	mynumber = sc.nextInt(2);
+	System.out.println("mothernumber:");
+	mothernumber = sc.nextDouble();
+
+
+    }
+}
